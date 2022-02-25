449bb36d24193dc3efbd7f1e876c660b7f3d26e3
mvisanu@gmail.com <mvisanu@gmail.com>
2022-02-25 13:12:59 -0600

    first commit

---
+++ ExpressoApi/Controllers/MenusController.cs
@@ -0,0 +1,29 @@
+﻿using ExpressoApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpressoApi.Controllers
+{
+    public class MenusController : ApiController
+    {
+        private ExpressoDbContext _dbContext = new ExpressoDbContext();
+
+        public IHttpActionResult getMenus()
+        {
+            var menus = _dbContext.Menus.Include("Submenus");
+
+            return Ok(menus);
+        }
+
+        public IHttpActionResult GetMenu(int id)
+        {
+            var menu = _dbContext.Menus.Include("Submenus").FirstOrDefault(x => x.Id == id);
+            if (menu == null) return BadRequest("not exisit");
+            return Ok(menu);
+        }
+    }
+}
---
+++ ExpressoApi/Controllers/ReservationsController.cs
@@ -0,0 +1,35 @@
+﻿using ExpressoApi.Data;
+using ExpressoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpressoApi.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+        private ExpressoDbContext _dbContext = new ExpressoDbContext();
+
+        public IHttpActionResult Post(Reservation reservations)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.Reservations.Add(reservations);
+            _dbContext.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.Created);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var reservations = _dbContext.Reservations;
+            if (reservations == null) return BadRequest("No reservation");
+            return Ok(reservations);
+        }
+    }
+}
